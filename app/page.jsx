5d841917391180b350e9d1d4278fd38135d95dea
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-04 19:37:16 +1000

    create home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,14 @@
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      
+      <h1 className="head_text text-center">Discover & Share</h1>
+      <br className="max-md:hidden"/>
+      <span className="orange_gradient text-center">Ai-Powered Prompts</span>
+      <p className="desc text-center">
+        promptify is an open-souce AI prompting tool for the modern world to discover, create and share creative prompts
+      </p>
+
+      {/* feed */}
     </section>
   )
 }
