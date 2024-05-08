c3b492f77aa89674d13481b89a8c111f94e735f9
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-08 13:54:49 +1000

    set up auth and database

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -6,77 +6,78 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
-  const [first, setFirst] = useState(false)
+  const { data: session } = useSession();
+
+  const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    }
-
-    setProviders()
-  }, [])
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap-2 flex-center">
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
-        src="/assets/images/logo.svg"
-        alt="Promptify Logo"
-        width={30}
-        height={30}
-        className="object-contain"
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
         />
-        <p className="logo_text">Promptify</p>
+        <p className='logo_text'>Promptify</p>
       </Link>
-      {/* desktop nav */}
-      <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">Create Post</Link>
-            <button
-            type="button"
-            onClick={signOut}
-            className="outline_btn"
-            >
+
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {session?.user ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
               Sign Out
             </button>
 
-            <Link href="/profile">
+            <Link href='/profile'>
               <Image
-              src="/assets/images/logo.svg"
-              alt="Profile"
-              width={37}
-              height={37}
-              className="rounded-full"
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
               />
             </Link>
           </div>
-        ): (
+        ) : (
           <>
-            {providers && 
+            {providers &&
               Object.values(providers).map((provider) => (
                 <button
-                type="button"
-                key={provider.name}
-                onClick={() => signIn(provider.id)}
-                className="black_btn"
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
         )}
       </div>
-      {/* mobile nav */}
+
+      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
@@ -131,10 +132,8 @@ const Nav = () => {
           </>
         )}
       </div>
-
-
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
