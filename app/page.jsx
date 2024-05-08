9a278a0c4a490c5657b4ad951c939b9467e62d83
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-08 14:10:54 +1000

    create prompt page

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,19 @@
-import Feed from "@components/Feed"
 
-const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">Discover & Share</h1>
-      <br className="max-md:hidden"/>
-      <span className="orange_gradient text-center">Ai-Powered Prompts</span>
-      <p className="desc text-center">
-        promptify is an open-souce AI prompting tool for the modern world to discover, create and share creative prompts
-      </p>
 
-      <Feed />
-    </section>
-  )
-}
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Promptify is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
 
-export default Home
+
+  </section>
+);
+
+export default Home;
